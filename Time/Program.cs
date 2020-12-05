b52dd2caa9ed456033c007e87c5b1b00756f2bcd
Wojtek Lisowicz <lisekmadridista@gmail.com>
2020-12-05 14:24:00 +0100

    dodany interfejs IEquatable do Time

--- Time/Program.cs
+++ Time/Program.cs
@@ -6,7 +6,18 @@ namespace Time
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Time t1 = new Time(12,0,59);
+            Time t11 = new Time(12);
+            Time t2 = new Time("00:44:02");
+            Time t22 = new Time("00:44:02");
+
+            var tt = t1.Equals(t11);
+            var tr = t2.Equals(t2);
+
+            Console.WriteLine(t1);
+            Console.WriteLine(t2);
+            Console.WriteLine(tt);
+            Console.WriteLine(tr);
         }
     }
 }
